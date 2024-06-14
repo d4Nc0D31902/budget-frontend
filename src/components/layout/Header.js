ad148c12a614339eb15f0e022d37933be2ff60ed
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-06-14 23:00:25 +0800

    Budget App features

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -41,11 +41,11 @@ const Header = () => {
   return (
     <AppBar position="sticky" color="success">
       <Toolbar>
-        {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
             Budget App
           </Link>
-        </Typography> */}
+        </Typography>
         <Box sx={{ flexGrow: 1 }} />
         {user ? (
           <div>
